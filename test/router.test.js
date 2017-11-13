576decd1418cd51e87c1d4eb8b6153e18a0ced97
Peter Marton <email@martonpeter.com>
2017-11-13 13:35:49 +0100

    fix(server): emit routed events, refactor router next, cover router with tests

--- test/router.test.js
+++ test/router.test.js
@@ -4,6 +4,7 @@
 /* eslint-disable func-names */
 
 var restify = require('../lib');
+var Router = require('../lib/router');
 var clients = require('restify-clients');
 
 if (require.cache[__dirname + '/lib/helper.js']) {
@@ -14,145 +15,22 @@ var helper = require('./lib/helper.js');
 ///--- Globals
 
 var test = helper.test;
-var mockResponse = function respond(req, res, next) {
-    res.send(200);
+var mockReq = {
+    params: {},
+    startHandlerTimer: function() {},
+    endHandlerTimer: function() {}
 };
-
-// TODO: cover with tests
-/* eslint-disable no-unreachable */
-return;
+var mockRes = {
+    setHeader: function() {},
+    send: function() {}
+};
 
 ///--- Tests
 
-test('render route', function(t) {
-    var server = restify.createServer();
-    server.get({ name: 'countries', path: '/countries' }, mockResponse);
-    server.get({ name: 'country', path: '/countries/:name' }, mockResponse);
-    server.get(
-        { name: 'cities', path: '/countries/:name/states/:state/cities' },
-        mockResponse
-    );
-
-    var countries = server.router.render('countries', {});
-    t.equal(countries, '/countries');
-
-    var country = server.router.render('country', { name: 'Australia' });
-    t.equal(country, '/countries/Australia');
-
-    var cities = server.router.render('cities', {
-        name: 'Australia',
-        state: 'New South Wales'
-    });
-    t.equal(cities, '/countries/Australia/states/New%20South%20Wales/cities');
-
-    t.end();
-});
-
-test('render route (missing params)', function(t) {
-    var server = restify.createServer();
-    server.get(
-        { name: 'cities', path: '/countries/:name/states/:state/cities' },
-        mockResponse
-    );
-
-    try {
-        server.router.render('cities', { name: 'Australia' });
-    } catch (ex) {
-        t.equal(ex, 'Error: Route <cities> is missing parameter <state>');
-    }
-
-    t.end();
-});
-
-test('GH #704: render route (special charaters)', function(t) {
-    var server = restify.createServer();
-    server.get({ name: 'my-route', path: '/countries/:name' }, mockResponse);
-
-    var link = server.router.render('my-route', { name: 'Australia' });
-    t.equal(link, '/countries/Australia');
-
-    t.end();
-});
-
-test('GH #704: render route (with sub-regex param)', function(t) {
-    var server = restify.createServer();
-    server.get(
-        {
-            name: 'my-route',
-            path: '/countries/:code([A-Z]{2,3})'
-        },
-        mockResponse
-    );
-
-    var link = server.router.render('my-route', { code: 'FR' });
-    t.equal(link, '/countries/FR');
-
-    link = server.router.render('my-route', { code: '111' });
-    t.equal(link, '/countries/111');
-    t.end();
-});
-
-test('GH-796: render route (with multiple sub-regex param)', function(t) {
-    var server = restify.createServer();
-    server.get(
-        {
-            name: 'my-route',
-            path: '/countries/:code([A-Z]{2,3})/:area([0-9]+)'
-        },
-        mockResponse
-    );
-
-    var link = server.router.render('my-route', { code: '111', area: 42 });
-    t.equal(link, '/countries/111/42');
-    t.end();
-});
-
-test('render route (with encode)', function(t) {
-    var server = restify.createServer();
-    server.get({ name: 'my-route', path: '/countries/:name' }, mockResponse);
-
-    var link = server.router.render('my-route', { name: 'Trinidad & Tobago' });
-    t.equal(link, '/countries/Trinidad%20%26%20Tobago');
-
-    t.end();
-});
-
-test('render route (query string)', function(t) {
-    var server = restify.createServer();
-    server.get({ name: 'country', path: '/countries/:name' }, mockResponse);
-
-    var country1 = server.router.render(
-        'country',
-        {
-            name: 'Australia'
-        },
-        {
-            state: 'New South Wales',
-            'cities/towns': 5
-        }
-    );
-
-    t.equal(
-        country1,
-        '/countries/Australia?state=New%20South%20Wales&cities%2Ftowns=5'
-    );
-
-    var country2 = server.router.render(
-        'country',
-        {
-            name: 'Australia'
-        },
-        {
-            state: 'NSW & VIC',
-            'cities&towns': 5
-        }
-    );
-
-    t.equal(
-        country2,
-        '/countries/Australia?state=NSW%20%26%20VIC&cities%26towns=5'
-    );
-
+test('sanitize url', function(t) {
+    t.equal(Router.sanitizeUrl('/foo'), '/foo');
+    t.equal(Router.sanitizeUrl('/foo/bar'), '/foo/bar');
+    t.equal(Router.sanitizeUrl('/foo?a=b&c=d'), '/foo');
     t.end();
 });
 
@@ -190,73 +68,157 @@ test('clean up xss for 404', function(t) {
     });
 });
 
-test('Strict routing handles root path', function(t) {
-    var server = restify.createServer({ strictRouting: true });
-    function noop() {}
-    server.get('/', noop);
+test('lookupByName runs a route by name and calls next', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    router.mount({ method: 'GET', path: '/', name: 'my-route' }, [
+        function(req, res, next) {
+            res.send('hello world');
+        }
+    ]);
 
-    var root = server.router.routes.GET[0];
-    t.ok(root.path.test('/'));
-
-    t.end();
-});
-
-test('Strict routing distinguishes trailing slash', function(t) {
-    var server = restify.createServer({ strictRouting: true });
-    function noop() {}
-
-    server.get('/trailing/', noop);
-    server.get('/no-trailing', noop);
-
-    var trailing = server.router.routes.GET[0];
-    t.ok(trailing.path.test('/trailing/'));
-    t.notOk(trailing.path.test('/trailing'));
-
-    var noTrailing = server.router.routes.GET[1];
-    t.ok(noTrailing.path.test('/no-trailing'));
-    t.notOk(noTrailing.path.test('/no-trailing/'));
-
-    t.end();
-});
-
-test('Default non-strict routing ignores trailing slash(es)', function(t) {
-    var server = restify.createServer();
-    function noop() {}
-
-    server.get('/trailing/', noop);
-    server.get('/no-trailing', noop);
-
-    var trailing = server.router.routes.GET[0];
-    t.ok(trailing.path.test('/trailing/'));
-    t.ok(trailing.path.test('//trailing//'));
-    t.ok(trailing.path.test('/trailing'));
-
-    var noTrailing = server.router.routes.GET[1];
-    t.ok(noTrailing.path.test('/no-trailing'));
-    t.ok(noTrailing.path.test('//no-trailing//'));
-    t.ok(noTrailing.path.test('/no-trailing/'));
-
-    t.end();
-});
-
-test('Find existing route with path', function(t) {
-    var server = restify.createServer();
-    function noop() {}
-
-    var routePath = '/route/:withParam';
-    server.get(routePath, noop);
-
-    var foundRoute = server.router.findByPath(
-        '/route/:withADifferentParamName',
-        { method: 'GET' }
+    router.lookupByName(
+        'my-route',
+        mockReq,
+        Object.assign({}, mockRes, {
+            send: function(data) {
+                t.equal(data, 'hello world');
+                t.end();
+            }
+        })
     );
-    t.equal(foundRoute.spec.path, routePath);
+});
 
-    var notFoundRoute = server.router.findByPath(
-        '/route/:withADifferentParamName([A-Z]{2,3})',
-        { method: 'GET' }
+test('lookupByName calls next with err', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    var myErr = new Error('My Error');
+    router.mount({ method: 'GET', path: '/', name: 'my-route' }, [
+        function(req, res, next) {
+            next(myErr);
+        }
+    ]);
+
+    router.lookupByName('my-route', mockReq, mockRes, function next(err) {
+        t.deepEqual(err, myErr);
+        t.end();
+    });
+});
+
+test('lookup runs a route chain by path and calls next', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    router.mount({ method: 'GET', path: '/', name: 'my-route' }, [
+        function(req, res, next) {
+            res.send('Hello world');
+            next(); // no _afterRoute without next()
+        }
+    ]);
+
+    router.lookup(
+        Object.assign({ method: 'GET', url: '/' }, mockReq),
+        mockRes,
+        function next(err) {
+            t.ifError(err);
+            t.end();
+        }
     );
-    t.notOk(notFoundRoute);
+});
 
-    t.end();
+test('lookup and sanitize', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    router.mount({ method: 'GET', path: '/', name: 'my-route' }, [
+        function(req, res, next) {
+            res.send('Hello world');
+            t.end();
+        }
+    ]);
+
+    router.lookup(
+        Object.assign({ method: 'GET', url: '/?a=b&c=d' }, mockReq),
+        mockRes,
+        function next() {}
+    );
+});
+
+test('lookup calls next with err', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    var myErr = new Error('My Error');
+    router.mount({ method: 'GET', path: '/', name: 'my-route' }, [
+        function(req, res, next) {
+            next(myErr);
+        }
+    ]);
+
+    router.lookup(
+        Object.assign({ method: 'GET', url: '/' }, mockReq),
+        mockRes,
+        function next(err) {
+            t.deepEqual(err, myErr);
+            t.end();
+        }
+    );
+});
+
+test('lookup emits routed when route found', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    router.mount({ method: 'GET', path: '/', name: 'my-route' }, [
+        function(req, res, next) {
+            res.send('Hello world');
+            next(); // no _afterRoute without next()
+        }
+    ]);
+
+    router.on('routed', function(req, res, route) {
+        t.end();
+    });
+
+    router.lookup(
+        Object.assign({ method: 'GET', url: '/' }, mockReq),
+        mockRes,
+        function next() {}
+    );
+});
+
+test('route handles 404', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    router.lookup(
+        Object.assign({ method: 'GET', url: '/' }, mockReq),
+        mockRes,
+        function next(err) {
+            t.equal(err.statusCode, 404);
+            t.end();
+        }
+    );
+});
+
+test('route handles method not allowed (405)', function(t) {
+    var router = new Router({
+        log: {}
+    });
+    router.mount({ method: 'GET', path: '/', name: 'my-route' }, [
+        function(req, res, next) {
+            res.send('Hello world');
+        }
+    ]);
+
+    router.lookup(
+        Object.assign({ url: '/', method: 'POST' }, mockReq),
+        mockRes,
+        function next(err) {
+            t.equal(err.statusCode, 405);
+            t.end();
+        }
+    );
 });
